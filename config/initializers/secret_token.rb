d2a6ebb3fc71ffcb0608bd6b30d2d4183d898dbe
Steve Arnold <steve@pointsandpixels.com>
2013-07-16 18:43:25 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purfekt::Application.config.secret_token = 'dd99ac372a52f467fb649dd1324a69c236337bc20f19e83011c35f16ea908974e5f68ef2900f2bb5347ea332919662ece177c851585372043aa4ae2e0dbee15b'

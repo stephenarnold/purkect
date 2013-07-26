2f6986902b9031a71190c35461c6d4af13e4681d
Steve Arnold <steve@pointsandpixels.com>
2013-07-26 12:27:54 -0700

    Add Remote Url Again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,14 +1,16 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image, :image_remote_url
 
+    def image_remote_url=(url_value)
+      self.image = URI.parse(url_value) unless url_value.blank?
+      super
+    end
+  
+  attr_accessible :description, :image, :image_remote_url
 
   	belongs_to :user
   	has_attached_file :image, styles: { medium: "320x240>"}
 
-  	def image_remote_url=(url_value)
-  		self.image = URI.parse(url_value) unless url_value.blank?
-  		super
-  	end
+
 
 		 validates :description, presence: true
 		 validates :user_id, presence: true
@@ -17,7 +19,6 @@ class Pin < ActiveRecord::Base
 		                            size: { less_than: 5.megabytes }
 
 
-  
 
 
 end

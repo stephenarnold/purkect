461a848d3fc20df613f178c65a3ae8f9c5157e18
Steve Arnold <steve@pointsandpixels.com>
2013-07-26 13:26:31 -0700

    Add Faker Gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,16 +1,13 @@
 class Pin < ActiveRecord::Base
+attr_accessible :description, :image, :image_remote_url
+
+  	belongs_to :user
+  	has_attached_file :image, styles: { medium: "320x240>"}
 
     def image_remote_url=(url_value)
       self.image = URI.parse(url_value) unless url_value.blank?
       super
     end
-  
-  attr_accessible :description, :image, :image_remote_url
-
-  	belongs_to :user
-  	has_attached_file :image, styles: { medium: "320x240>"}
-
-
 
 		 validates :description, presence: true
 		 validates :user_id, presence: true

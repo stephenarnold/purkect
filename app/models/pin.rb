c75dfa04881c1302dace21817f45d5e670759a25
Steve Arnold <steve@pointsandpixels.com>
2013-07-25 23:12:51 -0700

    Add Pins Scaffold

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,10 +1,11 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description
 
+
   validates :description, presence: true
+	validates :user_id, presence: true
 
   belongs_to :user
-
-  validates :user_id, presence: true
+  
 
 end
